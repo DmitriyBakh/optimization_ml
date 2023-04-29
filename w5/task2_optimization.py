653eddfb9640e9f25fcb9756b877681f8eea7309
Dmitrii <dmitrnv@gmail.com>
2023-04-30 01:29:35 +0300

    adding newton method

--- w5/task2_optimization.py
+++ w5/task2_optimization.py
@@ -242,6 +242,91 @@ def lbfgs(oracle, x_0, tolerance=1e-4, max_iter=500, memory_size=10,
     return x_k, message, history
 
 
+def newton(oracle, x_0, tolerance=1e-5, max_iter=100,
+           line_search_options=None, trace=False, display=False):
+    """
+    Newton's optimization method.
+
+    Parameters
+    ----------
+    oracle : BaseSmoothOracle-descendant object
+        Oracle with .func(), .grad() and .hess() methods implemented for computing
+        function value, its gradient and Hessian respectively. If the Hessian
+        returned by the oracle is not positive-definite method stops with message="newton_direction_error"
+    x_0 : np.array
+        Starting point for optimization algorithm
+    tolerance : float
+        Epsilon value for stopping criterion.
+    max_iter : int
+        Maximum number of iterations.
+    line_search_options : dict, LineSearchTool or None
+        Dictionary with line search options. See LineSearchTool class for details.
+    trace : bool
+        If True, the progress information is appended into history dictionary during training.
+        Otherwise None is returned instead of history.
+    display : bool
+        If True, debug information is displayed during optimization.
+
+    Returns
+    -------
+    x_star : np.array
+        The point found by the optimization procedure
+    message : string
+        'success' or the description of error:
+            - 'iterations_exceeded': if after max_iter iterations of the method x_k still doesn't satisfy
+                the stopping criterion.
+            - 'newton_direction_error': in case of failure of solving linear system with Hessian matrix
+                (e.g. non-invertible matrix).
+            - 'computational_error': in case of getting Infinity or None value during the computations.
+    history : dictionary of lists or None
+        Dictionary containing the progress information or None if trace=False.
+        Dictionary has to be organized as follows:
+            - history['time'] : list of floats, containing time passed from the start of the method
+            - history['func'] : list of function values f(x_k) on every step of the algorithm
+            - history['grad_norm'] : list of values Euclidian norms ||g(x_k)|| of the gradient
+                on every step of the algorithm
+            - history['x'] : list of np.arrays, containing the trajectory of the algorithm. ONLY STORE IF x.size <= 2
+
+    Example:
+    --------
+    >> oracle = QuadraticOracle(np.eye(5), np.arange(5))
+    >> x_opt, message, history = newton(oracle, np.zeros(5), line_search_options={'method': 'Constant', 'c': 1.0})
+    >> print('Found optimal point: {}'.format(x_opt))
+       Found optimal point: [ 0.  1.  2.  3.  4.]
+    """
+    history = defaultdict(list) if trace else None
+    line_search_tool = get_line_search_tool(line_search_options)
+    x_k = np.copy(x_0)
+
+    it = 0
+    start_time = time.time()
+    history = update_history(trace, display, history, oracle, 0, x_k, it)
+    grad_0 = oracle.grad(x_0)
+
+    while True:
+        grad_k = oracle.grad(x_k)
+        if np.linalg.norm(grad_k) ** 2 <= tolerance * np.linalg.norm(grad_0) ** 2:
+            return x_k, 'success', history
+
+        try:
+            c, lower = scipy.linalg.cho_factor(oracle.hess(x_k))
+            d_k = scipy.linalg.cho_solve((c, lower), -grad_k)
+        except LinAlgError:
+            return x_k, 'newton_direction_error', history
+
+        alpha = line_search_tool.line_search(oracle, x_k, d_k)
+        x_k = x_k + alpha * d_k
+        it += 1
+
+        history = update_history(trace, display, history, oracle, time.time() - start_time, x_k, it)
+
+        if (None in x_k) or (x_k > 10 ** 9).any():
+            return x_k, 'computational_error', history
+
+        if it > max_iter:
+            return x_k, 'iterations_exceeded', history
+        
+
 #######################################################
 #                                                     #
 #                     ORACLES                         #
